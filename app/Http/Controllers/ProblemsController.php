343f1f9522cd31c31c7f975e80ec4e6162ed28e0
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-20 18:22:10 +0300

    Add return type declarations to index and show methods in ProblemsController for improved type safety

--- app/Http/Controllers/ProblemsController.php
+++ app/Http/Controllers/ProblemsController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Problem;
 use App\Services\ProblemService;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProblemsController extends Controller
 {
-    public function index()
+    /**
+     * List all problems paginated with size Pagesize
+     *
+     * @return LengthAwarePaginator<int, Problem>
+     */
+    public function index(): LengthAwarePaginator
     {
         return ProblemService::getPageinated(15);
     }
 
-    public function show(Problem $problem)
+    /**
+     * Show problem by id
+     */
+    public function show(Problem $problem): Problem
     {
         return $problem;
     }
