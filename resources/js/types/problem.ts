d90da7bf902104d5eb1fa69e8734530e75ef5636
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-20 13:10:13 +0300

    Add missing id field to problem type definition

--- resources/js/types/problem.ts
+++ resources/js/types/problem.ts
@@ -3,6 +3,7 @@ export type problem = {
     url: string;
     title: string;
     description: string;
+    id: number;
     solution: string;
     runtime?: number; // milliseconds
     memory?: number; // megabytes
