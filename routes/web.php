9b2ab922fb27a2cf89d9a2412c2c1a6b59d4332f
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-20 15:00:20 +0300

    Refactor home route to use ProblemService for pagination and difficulty grouping

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProblemsController;
+use App\Services\ProblemService;
 use Illuminate\Support\Facades\Route;
 
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    $problems = (new ProblemsController())->index();
+    $problems = ProblemService::getPageinated(10);
+    $difficulty = ProblemService::groupByDifficulty();
     return Inertia::render('Welcome', [
         'problems' => $problems,
+        'difficulty' => $difficulty,
     ]);
 })->name('home');
 
