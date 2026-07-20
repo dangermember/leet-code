201cdf1e35232fc269780d54f591a353e3b3aa42
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-20 12:10:18 +0300

    Problem model

---
+++ app/Models/Problem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Problem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'problems';
+
+    protected $fillable = [
+        'number',
+        'url',
+        'title',
+        'description',
+        'solution',
+    ];
+
+    protected $casts = [
+        'number' => 'integer',
+    ];
+}
