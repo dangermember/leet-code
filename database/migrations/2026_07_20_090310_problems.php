5aad8b4860ab2ec913625f9e7988b1e3745c917b
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-20 12:07:43 +0300

    create problems

---
+++ database/migrations/2026_07_20_090310_problems.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create("problems", function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->unsignedBigInteger("number");
+            $table->string("url");
+            $table->string("title");
+            $table->string("description");
+            $table->string("solution");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
