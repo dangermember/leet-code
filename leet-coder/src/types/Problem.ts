589f1826ff1826d1c447e2870e88e17bfaea84e1
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-23 23:56:16 +0300

    feat: add Problem interface for defining problem structure

---
+++ leet-coder/src/types/Problem.ts
@@ -0,0 +1,17 @@
+import { Difficulty } from "./Difficulty";
+import { Topic } from "./Topic";
+
+export interface Problem {
+    id: number;
+    number: number;
+    url: string;
+    title: string;
+    description: string;
+    solution: string;
+    runtime: string | null;
+    memory: string | null;
+    difficulty: Difficulty;
+    created_at: string;
+    updated_at: string;
+    topics: Topic[];
+}
