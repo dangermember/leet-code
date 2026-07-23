870841d3d18e09f16867f4669d0c018ae29f8ea8
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-24 00:17:06 +0300

    Add favicon and logo SVGs; remove unused SVGs
    - Added a new favicon.ico file to the public directory.
    - Added a new logo.svg file to the public directory.
    - Removed unused SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.
    - Updated AppLogoIcon component to use the new logo.svg file.

--- leet-coder/src/components/app-logo-icon.tsx
+++ leet-coder/src/components/app-logo-icon.tsx
@@ -1,9 +1,8 @@
-import Logo from '@/app/assets/images/logo.svg'
 import Image from "next/image";
 
 export default function AppLogoIcon({ className, width = 50, height = 50 }: Readonly<{ className: string, width?: number, height?: number }>) {
     return <Image
-        src={Logo}
+        src={"/logo.svg"}
         alt="Logo"
         width={width}
         height={height}
