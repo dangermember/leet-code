aef268c1fed3a5ec55b5e18435ace4122303b6da
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-23 22:32:52 +0300

    add menuLinks constant for navigation links

---
+++ leet-coder/src/types/constants.ts
@@ -0,0 +1,10 @@
+export const menuLinks = [
+  { label: 'Home', href: "/" },
+  { label: 'Problems', href: "/problems" },
+  { label: 'Portfolio', href: 'https://mohamedeid.net', external: true },
+  {
+    label: 'Github',
+    href: 'https://github.com/dangermember',
+    external: true,
+  },
+];
